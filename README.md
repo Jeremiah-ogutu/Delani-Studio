745eb8a6489b913e802402f3cdd49cf5fdc8a9c4
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-04-19 12:13:25 +0300

    add readme and licence

---
+++ README.md
@@ -0,0 +1,20 @@
+# Delani-Studio
+## Author
+Jeremiah Juma
+## Description
+its a wedsite of Delani studio
+## setup and installation
+to access this project on your file youhave to
+install git 
+ on terminal $ sudo apt install git-all
+
+### Technologies used
+HTML
+CSS
+boostrap
+jquery
+javascript
+### Live site
+
+### licence
+this project is under the [MIT] (LINCEcE)
--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 $(document).ready(function(){
-  $("#picture").click(function(){
-    $("#p-graph").toggle();
-  });
-    $("#picture").click(function(){
-      $("#p-graph").toggle();
+  $("#design").click(function(){
+    $("#picture").fadeToggle();
+    $("#p-graph").toggle(function(click){
     });
-  
 });
+});
+
 $(document).ready(function(){
-  $("#picture2").click(function(){
-    $("#p1-graph").toggle();
-  });
-    $("#picture2").click(function(){
-      $("#p1-graph").toggle();
+  $("#development").click(function(){
+    $("#picture2").fadeToggle();
+    $("#p1-graph").toggle(function(click){
     });
-  
+});
+});
+
+$(document).ready(function(){
+  $("#product").click(function(){
+    $("#picture3").fadeToggle();
+    $("#p2-graph").toggle(function(click){
+    });
+});
 });
